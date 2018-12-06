e4016470899dc29d663a3ed4ae3bc6f14714a504
wade <b0231046@ems.niu.edu.tw>
2018-12-06 17:46:30 +0800

    V2 add show hide
    complete basic show/hide animate

--- js/Hogwarts.js
+++ js/Hogwarts.js
@@ -3,34 +3,48 @@
 
 $("#gry").click(
     function() {
-        // $(this).css({
-        //     "background-image": "url(https://i.imgur.com/EaI4UpW.jpg)",
-        //     "height": "580px",
-        //     "border-radius": "0%"
-        // })
-        $(this).removeClass("col-3")
-        $(this).removeClass("gry")
-        $(this).addClass("ani")
-        $(this).addClass("col-6")
-            // $(this).toggleClass("ani")
+        $(this).removeClass("col-3 gry p p_show")
+        $(this).addClass("col-6 gry_ani p_ani")
+        $("#rav,#huf,#sly").addClass("p_hide")
 
-        $(".HS,.rav").css("height", "0px")
+    }
+)
+
+$("#rav").click(
+    function() {
+        $(this).removeClass("col-3 rav p p_show")
+        $(this).addClass("col-6 rav_ani p_ani")
+        $("#gry,#huf,#sly").addClass("p_hide")
+
+    }
+)
+
+$("#huf").click(
+    function() {
+        $(this).removeClass("col-3 huf p p_show")
+        $(this).addClass("col-6 huf_ani p_ani")
+        $("#gry,#rav,#sly").addClass("p_hide")
+
+    }
+)
+
+$("#sly").click(
+    function() {
+        $(this).removeClass("col-3 sly p p_show")
+        $(this).addClass("col-6 sly_ani p_ani")
+        $("#gry,#rav,#huf").addClass("p_hide")
 
     }
 )
 
 $("div .col-2").click(
     function() {
-        // $(".gry").css({
-        //     "background-image": "url(https://i.imgur.com/9G7wc3v.jpg)",
-        //     "height": "280px",
-        //     "border-radius": "50%"
-        // })
-        $("#gry").removeClass("col-6")
-        $("#gry").removeClass("ani")
+        $("#gry,#rav,#huf,#sly").removeClass("col-6 gry_ani rav_ani huf_ani sly_ani p_ani p_hide")
+        $("#gry,#rav,#huf,#sly").addClass("col-3 p p_show")
         $("#gry").addClass("gry")
-        $("#gry").addClass("col-3")
-        $(".HS,.rav").css("height", "280px")
+        $("#rav").addClass("rav")
+        $("#huf").addClass("huf")
+        $("#sly").addClass("sly")
 
     }
 )
