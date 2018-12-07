5ed3e294f2f1503e616e15d7514b033de5abef0e
wade <b0231046@ems.niu.edu.tw>
2018-12-07 13:14:42 +0800

    V6 add back to home button
    add back to home button
    //push online

--- js/Hogwarts.js
+++ js/Hogwarts.js
@@ -4,6 +4,7 @@
 $("button").click(
     function() {
         $("#entrance").fadeOut(1000)
+        $(".home").hide()
     }
 )
 
@@ -17,6 +18,8 @@ $("#gry").click(
             $("#rav,#huf,#sly").addClass("p_hide")
                 // console.log("TT")
             $(this).html("<h5>葛來分多的始創人是高錐客·葛來分多（Godric Gryffindor），代表著勇氣、勇敢、膽量和騎士氣慨，代表顏色是猩紅色和金色，對應於火的元素。葛萊分多的吉祥物是獅子。學院負責人是變形學教授兼副校長麥米奈娃教授，直到她成為校長。學院的鬼魂是尼古拉斯·德米西·波普頓爵士，人稱「差點沒頭的尼克」。</h5>")
+
+            $(".homeGR").fadeIn()
         } else {}
 
     }
@@ -32,6 +35,8 @@ $("#rav").click(
             $("#gry,#huf,#sly").addClass("p_hide")
 
             $(this).html("<h5>雷文克勞的始創人是羅威娜·雷文克勞（Rowena Ravenclaw），代表著智慧，創造力，學習和穎悟力。代表顏色是藍色和古銅色（電影中是藍色和灰色），對應空氣的元素，而吉祥物是鷹。學院負責人是魔咒學的教授菲力·孚立維（Filius Flitwick），學院的鬼魂是雷文克勞的女兒海倫娜·雷文克勞（灰女士）。</h5>")
+
+            $(".homeGR").fadeIn()
         } else {}
 
     }
@@ -47,6 +52,8 @@ $("#huf").click(
             $("#gry,#rav,#sly").addClass("p_hide")
 
             $(this).html("<h5>赫夫帕夫的始創人是海加·赫夫帕夫（Helga Hufflepuff），代表著努力、耐心、正義和忠誠。代表顏色是鮮黃色和黑色，對應土的元素。赫夫帕夫的吉祥物是獾。學院負責人是草藥學教授帕莫娜·芽菜（Pomona Sprout）。學院的鬼魂是胖修士。</h5>")
+
+            $(".homeHS").fadeIn()
         } else {}
     }
 )
@@ -61,11 +68,14 @@ $("#sly").click(
             $("#gry,#rav,#huf").addClass("p_hide")
 
             $(this).html("<h5>史萊哲林的始創人是薩拉札·史萊哲林（Salazar Slytherin），代表著雄心勃勃，精明，領導才能及足智多謀。史萊哲林的吉祥物是蛇，代表顏色是綠色和銀色，對應的元素是水。在《哈利波特與神秘的魔法石》中，分類帽說史萊哲林的人會做任何事情來達到目的。賽佛勒斯·石內卜在《死神的聖物》前是史萊哲林的學院負責人，後來石內卜成為校長後由前學院負責人赫瑞司·史拉轟，在退休後重掌權力。學院的鬼魂是血腥男爵。</h5>")
+
+            $(".homeHS").fadeIn()
         } else {}
     }
 )
 
-$("div .col-2").click(
+// "div .col-2"
+$(".home").click(
     function() {
         $("#gry,#rav,#huf,#sly").removeClass("col-6 gry_ani rav_ani huf_ani sly_ani p_ani p_hide")
         $("#gry,#rav,#huf,#sly").addClass("col-3 p p_show")
@@ -75,5 +85,7 @@ $("div .col-2").click(
         $("#sly").addClass("sly")
 
         $("#gry,#rav,#huf,#sly").html("")
+
+        $(".home").fadeOut()
     }
 )
